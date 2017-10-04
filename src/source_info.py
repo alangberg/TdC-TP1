189c689252b5e85cb6eb436fdccb1463fcc03890
Fernando Balboa <fbalboa95@gmail.com>
2017-10-04 13:50:08 -0300

    Version mejorada de la herramienta, extensible a mas tipos de fuentes. Nuevo getProtocol() dinamico.

--- src/source_info.py
+++ src/source_info.py
@@ -3,19 +3,6 @@ from scapy.all import *
 from math import log
 import sys, os
 
-input_file = "sniffer_output.pcap"
-
-params_count = len(sys.argv)
-if params_count > 1:
-	input_file = sys.argv[1]
-	if params_count > 2:
-		print "Use: python source_info.py [input_file]=sniffer_output.pcap"
-
-#Simbolo = < unicast/broadcast, ARP/IP/IPv6/?? >
-packets_count = len(rdpcap(input_file))  #Reads all the packets of the pcap file
-broadcast_addr = 'ff:ff:ff:ff:ff:ff'
-
-
 def is_unicast_or_broadcast(pkt):
 	if pkt.dst == broadcast_addr:
 		return 'broadcast'
@@ -23,14 +10,10 @@ def is_unicast_or_broadcast(pkt):
 		return 'unicast'
 
 def get_protocol(pkt):
-	if ARP in pkt:
-		protocol = 'ARP'
-	elif IPv6 in pkt:
-		protocol = 'IPv6'
-	elif IP in pkt:
-		protocol = 'IP'
-
-	return protocol
+	#If it has a superior layer, return it
+	if pkt.payload:
+		pkt = pkt.payload
+		return pkt.name
 
 def analyze_pkt_S1(pkt):
 	global symbols_S1
@@ -50,7 +33,7 @@ def source_symbol_probabilities(symbols):
 
 class Source_S1(object):
 	
-	def __init__(self, symbols, name):
+	def __init__(self, symbols, name='S1'):
 		self.probabilities = source_symbol_probabilities(symbols)
 		self.symbols = self.probabilities.keys()
 		self.name = name
@@ -67,7 +50,7 @@ class Source_S1(object):
 	def name(self):
 		return self.name
 
-	def probabilities(self):
+	def symbol_probabilities(self):
 		return self.probabilities
 
 	def print_probabilities(self):
@@ -84,10 +67,69 @@ class Source_S1(object):
 		print "{0}'s entropy is: {1}".format(self.name, self.entropy()) 
 		print "{0}'s maximum entropy is: {1}".format(self.name, self.max_entropy())
 		
+#Dump source info to a csv file, with sep = ";"
+def source_to_csv(output_file, source):
+	output_file += ".csv"
+	probabilities = source.symbol_probabilities()
+	#List of tuples with (uni_or_broad, protocol, probability, information)
+	symbols_info = [(k[0], k[1], str(v), str(-log(v, 2))) for k, v in probabilities.iteritems()]
+	symbols_header = "Symbol; Probability; Information\n"
+	with open(output_file, 'w') as f:
+		#Print header and info of each source symbol
+		f.write(symbols_header)
+		for i in range(len(symbols_info)):
+			si = symbols_info[i]
+			f.write("({0}, {1}); {2}; {3}\n".format(si[0], si[1], si[2], si[3]))
+
+		f.write("\n\n") #Separator
+		entropy_header = "Entropy; Maximum Entropy\n"
+		f.write(entropy_header)
+		f.write("{0};{1}".format(str(source.entropy()), str(source.max_entropy())))
+
+def pcap_table_S1(output_file):
+	global symbols_S1
+	global input_file
+	print "Reading input pcap file."
+	sniff(prn=analyze_pkt_S1, offline=input_file, count=packets_count, store=0)  #Read pcap file
+	print "Done. Creating S1 source."
+	source_S1 = Source_S1(symbols_S1)
+	print "Done. Dumping source info to {0}_S1.csv".format(output_file)
+	#source_S1.print_source_info()
+	source_to_csv(output_file + "_S1", source_S1)
+
+
+invalid_args_error = """ Use: python source_info.py [input_file]=sniffer_output.pcap [outputfile]=source_info [source_type]=1\n
+					Example: python source_info.py homeNetwork.pcap homeNetwork 1\n
+					Creates homeNetwork_S1.csv with the info from the source created by modelling homeNetwork.pcap as a S1 source.
+					"""
 
 symbols_S1 = {}
-print "Reading input pcap file."
-sniff(prn=analyze_pkt_S1, offline=input_file, count=packets_count, store=0)  #Read pcap file
-print "Done. Creating S1 source."
-source_S1 = Source_S1(symbols_S1, 'S1')
-source_S1.print_source_info()
+symbols_S2 = {}
+	
+if __name__ == "__main__":
+
+	input_file = "sniffer_output.pcap"
+	output_file = "source_info"
+	source_type = 1
+
+	params_count = len(sys.argv)
+	if params_count > 1:
+		input_file = sys.argv[1]
+		if params_count > 2:
+			output_file = sys.argv[2]
+			if params_count > 3:
+				source_type = int(sys.argv[3])
+				if params_count > 4:
+					print invalid_args_error
+
+	#Simbolo = < unicast/broadcast, ARP/IP/IPv6/?? >
+	packets_count = len(rdpcap(input_file))  #Reads all the packets of the pcap file
+	broadcast_addr = 'ff:ff:ff:ff:ff:ff'
+
+	if source_type == 1:
+		pcap_table_S1(output_file)
+	elif source_type == 2:
+		#TODO
+		pass
+	else:
+		print "source_type can only be 1 or 2"
