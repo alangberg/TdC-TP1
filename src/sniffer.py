68ec4e5d33d3af13a71caedc063d962cc0768f74
Fernando Balboa <fbalboa95@gmail.com>
2017-10-05 11:38:36 -0300

    Fix en sniffer

--- src/sniffer.py
+++ src/sniffer.py
@@ -5,18 +5,18 @@ import sys, os
 
 packets_count = 15000
 output_file = "sniffer_output.pcap"
-timeout = 30 * 60		#30 minutes
+timeout = 60 * 60		#1 hour
 
 
 params_count = len(sys.argv)
 if params_count >= 2:
 	packets_count = int(sys.argv[1])
 	if params_count >= 3:
-		output_file = sys.arg[v2]
+		output_file = sys.argv[v2]
 		if params_count == 4:
 			timeout = int(sys.argv[3])
 	if params_count > 4:
-		print "Use: sudo python sniffer.py [packets count]=15000 [output file]=sniff_output.pcap [timeout]=30min "
+		print "Use: sudo python sniffer.py [packets count]=15000 [output file]=sniff_output.pcap [timeout]=1hour "
 
 
 #pkts = sniff(filter='arp', store=1, count=packets_count, timeout=timeout) Agarra solo los ARP
